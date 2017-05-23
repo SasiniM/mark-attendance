927ea152f4bd7479e79d4389d5bf95abc6d6dafc
amila1991 <amilakarunathilaka.12@cse.mrt.ac.lk>
2017-05-23 23:23:16 +0530

    add Company repository

---
+++ src/main/java/org/dialog/hack/repository/CompanyRepository.java
@@ -0,0 +1,10 @@
+package org.dialog.hack.repository;
+
+import org.dialog.hack.model.Company;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author amila karunathilaka.
+ */
+public interface CompanyRepository extends CrudRepository <Company, Integer> {
+}
