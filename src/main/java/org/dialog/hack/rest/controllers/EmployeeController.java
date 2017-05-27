52fa6afac71a64aec41f067cdbc31b4b8eb7fd9f
amila1991 <amilakarunathilaka@gmail.com>
2017-05-27 14:45:43 +0530

    create employee Employee Controller Implementation

--- src/main/java/org/dialog/hack/rest/controllers/EmployeeController.java
+++ src/main/java/org/dialog/hack/rest/controllers/EmployeeController.java
@@ -1,12 +1,10 @@
 package org.dialog.hack.rest.controllers;
 
+import org.dialog.hack.rest.resources.EmployeeProfileResource;
 import org.dialog.hack.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -25,4 +23,13 @@ public class EmployeeController {
     public Response getEmployeeProfiles(@PathParam("firstname") String firstName, @PathParam("lastname") String lastName){
         return Response.status(200).entity(employeeService.getemployeeProfiles(firstName, lastName)).build();
     }
+
+    @PUT
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createEmployeeProfile(EmployeeProfileResource profileResource) {
+        return Response.status(200).entity("").build();
+    }
+
 }
