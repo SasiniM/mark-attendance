13711ea9f9decbf30446d6cfa5bbe728af8ffea4
amila1991 <amilakarunathilaka@gmail.com>
2017-05-28 18:15:43 +0530

    refactor get specification

--- src/main/java/org/dialog/hack/repository/specification/employeeattendance/EmployeeAttendanceGetSpecification.java
+++ src/main/java/org/dialog/hack/repository/specification/employeeattendance/EmployeeAttendanceGetSpecification.java
@@ -7,26 +7,27 @@
 package org.dialog.hack.repository.specification.employeeattendance;
 
 import org.dialog.hack.model.EmployeeAttendance;
+import org.dialog.hack.model.EmployeeProfile;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.util.Date;
+import java.sql.Date;
 
 /**
  * @author Amila Karunathilaka
  * @since 17.07
  */
-public class EmployeeAttendanceSpecification implements Specification<EmployeeAttendance> {
+public class EmployeeAttendanceGetSpecification implements Specification<EmployeeAttendance> {
 
     private final Date startDate;
     private final Date endDate;
     private final String firstName;
     private final String lastName;
 
-    public EmployeeAttendanceSpecification(Date startDate, Date endDate, String firstName, String lastName) {
+    public EmployeeAttendanceGetSpecification(Date startDate, Date endDate, String firstName, String lastName) {
         this.startDate = startDate;
         this.endDate = endDate;
         this.firstName = firstName;
@@ -35,8 +36,8 @@ public class EmployeeAttendanceSpecification implements Specification<EmployeeAt
 
     @Override
     public Predicate toPredicate(Root<EmployeeAttendance> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
-        return cb.and(cb.between(root.get("attendanceDate"), startDate, endDate),
-                cb.and(cb.equal(root.get("employeeProfile").get("empFirstname"), firstName),
-                        cb.equal(root.get("employeeProfile").get("empLastname"), lastName)));
+        return cb.and(cb.between(root.<Date>get("attendanceDate"), startDate, endDate),
+                cb.and(cb.equal(root.<EmployeeProfile>get("employeeProfile").get("empFirstname"), firstName),
+                        cb.equal(root.<EmployeeProfile>get("employeeProfile").get("empLastname"), lastName)));
     }
 }
