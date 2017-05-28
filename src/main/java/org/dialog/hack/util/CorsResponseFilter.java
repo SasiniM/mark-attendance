bf1f44aba1a8efc3b83635e588a768d2ef9aebb0
amila1991 <amilakarunathilaka@gmail.com>
2017-05-28 18:22:30 +0530

    add Cors Response Filter

---
+++ src/main/java/org/dialog/hack/util/CorsResponseFilter.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2010-2017 AdroitLogic Private Ltd. (https://www.adroitlogic.com). All Rights Reserved.
+ *
+ * AdroitLogic PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package org.dialog.hack.util;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
+
+/**
+ * @author Amila Karunathilaka
+ * @since 17.07
+ */
+public class CorsResponseFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext,
+                       ContainerResponseContext containerResponseContext) throws IOException {
+        MultivaluedMap<String, Object> headers = containerResponseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia, Authorization");
+        //headers.add("access-control-expose-headers", "Authorization");
+    }
+}
