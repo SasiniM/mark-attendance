90403c8a7d9a495804b179fd7918b0b1701e982c
amila1991 <amilakarunathilaka@gmail.com>
2017-05-28 18:34:45 +0530

    attendance controller refactoring

--- src/main/java/org/dialog/hack/rest/controllers/AttendanceController.java
+++ src/main/java/org/dialog/hack/rest/controllers/AttendanceController.java
@@ -1,12 +1,12 @@
 package org.dialog.hack.rest.controllers;
 
-import org.dialog.hack.model.EmployeeAttendance;
-import org.dialog.hack.rest.resources.EmployeeAttendanceResource;
-import org.dialog.hack.rest.resources.EmployeeProfileResource;
 import org.dialog.hack.service.AttendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -22,20 +22,20 @@ public class AttendanceController {
     @GET
     @Path("/{year}/{month}/{firstname}/{lastname}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getEmployeeAttendances(@PathParam("year") String year, @PathParam("month") String month, @PathParam("firstname") String firstName, @PathParam("lastname") String lastName){
+    public Response getEmployeeAttendances(@PathParam("year") String year, @PathParam("month") String month, @PathParam("firstname") String firstName, @PathParam("lastname") String lastName) {
         return Response.status(200).entity(attendanceService.getEmployeeAttendances(year, month, firstName, lastName)).build();
     }
 
-@GET
-@Path("/{username}")
-@Produces(MediaType.APPLICATION_JSON)
-public Response createEmployeeAttendance(@PathParam("username") String username) {
-    boolean isSuccess = attendanceService.saveEmployeeAttendance(username);
+    @GET
+    @Path("/{username}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createEmployeeAttendance(@PathParam("username") String username) {
+        boolean isSuccess = attendanceService.saveEmployeeAttendance(username);
 
-    if (isSuccess == true)
-        return Response.status(200).entity("Transaction successful").build();
-    else
-        return Response.status(200).entity("Transaction faild").build();
+        if (isSuccess == true)
+            return Response.status(200).entity("Transaction successful").build();
+        else
+            return Response.status(200).entity("Transaction faild").build();
 
-}
+    }
 }
